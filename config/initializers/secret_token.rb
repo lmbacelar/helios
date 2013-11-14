2b78998c701cb67ace6b85f51e9a352aef32790f
Luis Bacelar <lmbacelar@gmail.com>
2013-11-14 13:47:50 +0000

    initial commit wit test setup for rspec, cucumber-rails, capybara, autotest and simplecov

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helios::Application.config.secret_key_base = '25d738b4d662d7f53cbd9aec528bbcae79dabbbddbd816364e85d183515c898cd4ad7cfb99f88cd7b31f753015cc5c2fb55ac849b47edc2729ebf4989beabc14'
