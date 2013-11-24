c6ca10f76690f5a1a6eb01a7d8e68a5d1375fd1d
Luis Bacelar <lmbacelar@gmail.com>
2013-11-24 03:04:19 +0000

    poetry mode on function calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def present(object, klass = nil)
+  def present object, klass = nil
     klass ||= "#{object.class}Presenter".constantize
-    presenter = klass.new(object, self)
+    presenter = klass.new object, self
     yield presenter if block_given?
     presenter
   end
