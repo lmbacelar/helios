9b66328fc328d5906fc34de8737c3f1cd707a4c8
Luis Bacelar <lmbacelar@gmail.com>
2013-11-24 03:45:52 +0000

    Routing Its90Prt measures index to Measurements Controller, remaining actions to PrtMeasurementsController. DRY route creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Helios::Application.routes.draw do
-  resources :its90_prts do
-    resources :measurements
-  end
-  resources :iec60751_prts do
+  resources :its90_prts, :iec60751_prts do
     resources :measurements, only: :index
     resources :prt_measurements, except: :index
   end
